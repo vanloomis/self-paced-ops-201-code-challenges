41d9eecfdc2cec5cc913978ac282e16551130ee3
vanloomis <vanloomis40@gmail.com>
2023-12-11 00:24:34 -0500

    helloworld

---
+++ helloworld.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script Name:  Challenge 01
+# Author:  Donovan Loomis
+# Date of Latest Version:  12/03/23
+# purpose:  Test
+
+var="Everything is working perfectly"
+echo "Hello World"
+echo $var
