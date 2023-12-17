e2a40f080f746f84c39209061b67faf18c860a2a
vanloomis <vanloomis40@gmail.com>
2023-12-17 17:53:37 -0500

    This is my login history

---
+++ userlogin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script Name:  Challenge 02
+# Author:  Donovan Loomis
+# Date of Latest Version:  12/16/23
+# purpose:  user login tracker
+user_login (){
+last
+     echo "This is my login history"
+}
+
+user_login
+user_login
+user_login
